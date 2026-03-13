a21b64a2d87f4a002059121070b2b1a9dd08ec41
Isaprez <105239192+Isaprez@users.noreply.github.com>
2026-03-13 10:07:51 -0600

    Initial Commit

---
+++ Session/SessionApp.swift
@@ -0,0 +1,17 @@
+//
+//  SessionApp.swift
+//  Session
+//
+//  Created by Isai Pérez Rojas on 13/03/26.
+//
+
+import SwiftUI
+
+@main
+struct SessionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
